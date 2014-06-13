7901fdaf1d621fe85c7ae88f1bc1e397e1d1769e
Basheer Subei <basheersubei@gmail.com>
2014-06-13 17:41:34 -0500

    created artist model and migrations and scaffold (views and controller)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ArtsicleApp::Application.routes.draw do
+  resources :artists
   resources :artworks
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140613221014) do
+ActiveRecord::Schema.define(version: 20140613223527) do
 
   create_table "artists", force: true do |t|
     t.string   "name"
